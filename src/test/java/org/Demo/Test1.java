26b7237949eb4278bf5e9ae79a55728c7e3c142f
Akash <akash46.choudhary@gmail.com>
2022-12-03 13:25:52 +0530

    first commit

---
+++ src/test/java/org/Demo/Test1.java
@@ -0,0 +1,23 @@
+package org.Demo;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+
+	
+	@Test
+	public void verifyTest() {
+		
+		System.out.println("hello its run");
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
