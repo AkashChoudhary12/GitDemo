eb9a315946d55cc3dbdb461203280a46d4a1030a
Akash <akash46.choudhary@gmail.com>
2022-12-03 16:38:41 +0530

    Architect Asian development

---
+++ src/test/java/org/Demo/Branch_Test.java
@@ -0,0 +1,19 @@
+package org.Demo;
+
+public class Branch_Test {
+
+	
+	
+	public void branchDemotest() {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("branch test is pass");
+		
+		
+	}
+	
+	
+	
+	
+}
